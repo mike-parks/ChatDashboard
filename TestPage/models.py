57dc7491494987c5bbe41c5676a6674ded755a8f
Trevdor <trevor.c.haag+github1@gmail.com>
2014-06-07 15:39:21 -0500

    Adding MongoEngine Model
    Created basic model class to link MongoDB to Django.

--- TestPage/models.py
+++ TestPage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import mongoengine as mdbeng
+
+from TestSite2 import DBNAME
 
 # Create your models here.
+mdbeng.connect(DBNAME)
+
+class Post(Document):
+    username = mdbeng.StringField(max_length=50, required=True)
+    message = mdbeng.StringField(max_length=10000, required=True)
