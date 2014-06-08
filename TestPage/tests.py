b0b2b5d3d8aa734c72bc9f444390862f3ddc9b0d
skoolhouseteach <bulls_eye_99@yahoo.com>
2014-06-08 19:50:44 -0400

    Test cases 1.1 - 1.4

--- TestPage/tests.py
+++ TestPage/tests.py
@@ -22,6 +22,182 @@ class MongoTestCase(TestCase):
             self.db.drop_collection(collection)
 
 
+
+class TestCase1Pt1(MongoTestCase):
+    def setUp (self):
+        self.factory = RequestFactory()
+        
+    def test_register(self):
+        # registers user and then checks that user exists
+        # create username with timestamp to make sure they are unique
+        now = datetime.datetime.now()
+        username = "TestUser-%d%d%d%d%d%d" % (now.year, now.month, now.day, now.hour, now.minute, now.second)
+        
+        # register user
+        request = self.factory.post('/Register')
+        request.POST['username'] = username
+        request.POST['password'] = "Password1"
+        request.POST['email'] = "testUser@email.com"
+        response = views.send_message(request)
+        self.assertEqual(response.status_code, 200, "Was unable to register user.")
+        
+        
+        # attempt to register user a second time - should fail        
+        response = views.send_message(request)
+        self.assertNotEqual(response.status_code, 200, "Was able to register duplicate user.")
+        
+        
+        #recreate username because the first one is already used
+        now = datetime.datetime.now()
+        username = "TestUser2-%d%d%d%d%d%d" % (now.year, now.month, now.day, now.hour, now.minute, now.second)
+        
+        # fail to register user with invalid password format - no number
+        request = self.factory.post('/Register')
+        request.POST['username'] = username
+        request.POST['password'] = "Password"
+        request.POST['email'] = "testUser@email.com"
+        response = views.send_message(request)
+        self.assertNotEqual(response.status_code, 200, "Was able to register user with invalid password format - no number.")
+        
+        # fail to register user with invalid password format- no uppercase
+        request = self.factory.post('/Register')
+        request.POST['username'] = username
+        request.POST['password'] = "password1"
+        request.POST['email'] = "testUser@email.com"
+        response = views.send_message(request)
+        self.assertNotEqual(response.status_code, 200, "Was able to register user with invalid password format - no uppercase.")
+        
+        # fail to register user with invalid password format - no lower case
+        request = self.factory.post('/Register')
+        request.POST['username'] = username
+        request.POST['password'] = "PASSWORD1"
+        request.POST['email'] = "testUser@email.com"
+        response = views.send_message(request)
+        self.assertNotEqual(response.status_code, 200, "Was able to register user with invalid password format - no lowercase.")
+        
+        # fail to register user with invalid email format
+        request = self.factory.post('/Register')
+        request.POST['username'] = username
+        request.POST['password'] = "Password1"
+        request.POST['email'] = "testUser"
+        response = views.send_message(request)
+        self.assertNotEqual(response.status_code, 200, "Was able to register user with invalid email format.")
+        
+        # fail to register user without email
+        request = self.factory.post('/Register')
+        request.POST['username'] = username
+        request.POST['password'] = "Password1"
+        request.POST['email'] = ""
+        response = views.send_message(request)
+        self.assertNotEqual(response.status_code, 200, "Was able to register user without email.")
+        
+        
+        
+class TestCase1Pt2(MongoTestCase):
+    def setUp (self):
+        self.factory = RequestFactory()
+        
+    def test_login(self):
+        username = "TestUser"
+        
+        # log user in
+        request = self.factory.post('/Login')
+        request.POST['username'] = username
+        request.POST['password'] = "Password1"
+        response = views.send_message(request)
+        self.assertEqual(response.status_code, 200, "Was unable to log user in.")
+        
+        # fail to log user in with wrong password
+        request = self.factory.post('/Login')
+        request.POST['username'] = username
+        request.POST['password'] = "BadPassword"
+        response = views.send_message(request)
+        self.assertNotEqual(response.status_code, 200, "Was able to log user in with bad password.") 
+        
+        # fail to log user in with wrong username
+        request = self.factory.post('/Login')
+        request.POST['username'] = "BadTestUser"
+        request.POST['password'] = "Password1"
+        response = views.send_message(request)
+        self.assertNotEqual(response.status_code, 200, "Was able to log user in with invalid username.") 
+        
+        
+class TestCase1Pt3(MongoTestCase):
+    def setUp (self):
+        self.factory = RequestFactory()
+        
+    def test_logout(self):
+        # log user in
+        request = self.factory.post('/Logout')
+        request.POST['username'] = username
+        response = views.send_message(request)
+        self.assertEqual(response.status_code, 200, "Was unable to log user out.")
+        
+        
+class TestCase1Pt4(MongoTestCase):
+    def setUp (self):
+        self.factory = RequestFactory()
+        
+    def test_changePassword(self):
+        username = "TestUser"
+        
+        # change password
+        request = self.factory.post('/ChangePassword')
+        request.POST['username'] = "TestUser"
+        request.POST['CurrentPassword'] = "password"
+        request.POST['NewPassword'] = "Password2"
+        response = views.send_message(request)
+        self.assertEqual(response.status_code, 200, "Was unable to change password.") 
+        
+        # fail change password - wrong current password        
+        request = self.factory.post('/ChangePassword')
+        request.POST['username'] = "TestUser"
+        request.POST['CurrentPassword'] = "BadPassword"
+        request.POST['NewPassword'] = "Password2"
+        response = views.send_message(request)
+        self.assertEqual(response.status_code, 200, "Was able to change password with invalid Current Password.") 
+        
+        #fail change password - wrong username        
+        request = self.factory.post('/ChangePassword')
+        request.POST['username'] = "BadTestUser"
+        request.POST['CurrentPassword'] = "Password2"
+        request.POST['NewPassword'] = "Password2"
+        response = views.send_message(request)
+        self.assertNotEqual(response.status_code, 200, "Was able to change password on BadTestUser.") 
+        
+        
+        # fail on change password to password without number
+        request = self.factory.post('/ChangePassword')
+        request.POST['username'] = "TestUser"
+        request.POST['CurrentPassword'] = "Password2"
+        request.POST['NewPassword'] = "Password"
+        response = views.send_message(request)
+        self.assertNotEqual(response.status_code, 200, "Was able to change password to one without a number.") 
+        
+        # fail on change password to password without uppercase alpha
+        request = self.factory.post('/ChangePassword')
+        request.POST['username'] = "TestUser"
+        request.POST['CurrentPassword'] = "Password2"
+        request.POST['NewPassword'] = "password1"
+        response = views.send_message(request)
+        self.assertNotEqual(response.status_code, 200, "Was able to change password to one without an uppercase alpha.") 
+        
+        # fail on change password to password without lowercase alpha
+        request = self.factory.post('/ChangePassword')
+        request.POST['username'] = "TestUser"
+        request.POST['CurrentPassword'] = "Password2"
+        request.POST['NewPassword'] = "PASSWORD1"
+        response = views.send_message(request)
+        self.assertNotEqual(response.status_code, 200, "Was able to change password to one without a lowecase alpha.") 
+    
+        # change password back for next test
+        request = self.factory.post('/ChangePassword')
+        request.POST['username'] = "TestUser"
+        request.POST['CurrentPassword'] = "Password2"
+        request.POST['NewPassword'] = "Password1"
+        response = views.send_message(request)
+        self.assertEqual(response.status_code, 200, "Was unable to change password back to origional password.") 
+
 class TestCase2Pt1(MongoTestCase):
 
     def test_connectivity(self):
