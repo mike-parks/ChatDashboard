4d4af893481b49dcfb4af29d8a89f1d53923bc86
Team1ASESSWENG500 <tch161@psu.edu>
2014-06-14 16:44:50 -0400

    Fixed importing chatapp issue

--- ChatDashboard/chatapp/urls.py
+++ ChatDashboard/chatapp/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from chatapp import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ChatDashboard.views.home', name='home'),
