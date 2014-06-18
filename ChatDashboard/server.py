c359bb3f619aea157281f029c3b22b7a06ac2c14
Team1ASESSWENG500 <tch161@psu.edu>
2014-06-17 23:16:34 -0400

    First Twisted server commit
    It works over telnet, 127.0.0.1 1025. Still needs work to be fully
    featured as a chat server.

---
+++ ChatDashboard/server.py
@@ -0,0 +1,34 @@
+from sets import Set
+from twisted.internet import reactor, protocol
+from twisted.protocols import basic
+
+"""Creating Twisted protocol class.
+Requires these functions: connectionMade, connectionLost, lineReceived."""
+class DashboardProtocol(basic.LineReceiver):
+
+    def __init__(self, factory):
+        self.factory = factory
+
+    def connectionMade(self):
+        self.factory.clients.add(self)
+
+    def connectionLost(self, reason):
+        self.factory.clients.remove(self)
+
+    def lineReceived(self, line):
+        for c in self.factory.clients:
+            c.sendLine("<{}> {}".format(self.transport.getHost(), line))
+
+"""Factory for DashboardProtocol class.
+Each instance will store its own unique set of connected clients."""
+class DashboardProtocolFactory(protocol.Factory):
+
+    def __init__(self):
+        self.clients = set()
+
+    def buildProtocol(self, addr):
+        return DashboardProtocol(self)
+
+reactor.listenTCP(1025, DashboardProtocolFactory())
+reactor.run()
+
