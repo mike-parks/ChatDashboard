ce785808e422094ee648dbfe5a9ce349d3b06620
Team1ASESSWENG500 <tch161@psu.edu>
2014-06-21 15:19:22 -0400

    Adding message model, postable messages on a chat dashboard

--- ChatDashboard/chatapp/models.py
+++ ChatDashboard/chatapp/models.py
@@ -1,6 +1,11 @@
 #from django.db import models
-from mongoengine import Document, StringField
+from mongoengine import Document, StringField, DateTimeField
 
 # Create your models here.
 class Dashboard(Document):
     title = StringField(max_length=200, primary_key=True)
+
+class Message(Document):
+    msgtext = StringField(max_length=200)
+    dashboardtitle = StringField(max_length=200)
+    timestamp = DateTimeField()
--- ChatDashboard/chatapp/views.py
+++ ChatDashboard/chatapp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import HttpResponse
 from django.template import loader, RequestContext
 from mongoengine.django.shortcuts import get_document_or_404
 
-from models import Dashboard
+from models import Dashboard, Message
+import datetime
 
 # Create your views here.
 def list(request):
@@ -20,9 +21,17 @@ def list(request):
     return HttpResponse(template.render(context))
 
 def render_dashboard(request, title):
+    if request.method == "POST":
+        message = request.POST.get('message', '')
+        dashboard_title = request.POST['dashboard_title']
+        message = Message(msgtext=message, timestamp=datetime.datetime.now(), dashboardtitle=dashboard_title)
+        message.save()
+
+    messages_here = Message.objects(dashboardtitle=title)
     template = loader.get_template('dashboard.html')
     dashboard = get_document_or_404(Dashboard, pk=title)
     context = RequestContext(request, {
+        'all_messages': messages_here,
         'dashboard': dashboard
     })
     return HttpResponse(template.render(context))
