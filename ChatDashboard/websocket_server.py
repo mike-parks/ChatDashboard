a2c30333c3e312352bbef331bb9ef4b8341f5eb7
Team1ASESSWENG500 <tch161@psu.edu>
2014-06-28 00:49:53 -0400

    Chat is working
    Still need to understand lineReceived vs dataReceived vs rawDataReceived

--- ChatDashboard/websocket_server.py
+++ ChatDashboard/websocket_server.py
@@ -9,17 +9,26 @@ Requires these functions: connectionMade, connectionLost, lineReceived."""
 class DashboardProtocol(basic.LineReceiver):
 
     def __init__(self, factory):
+        self.setLineMode()
         self.factory = factory
 
     def connectionMade(self):
         self.factory.clients.add(self)
+        print "Connected. Connected clients: " + str(self.factory.clients)
+        #for c in self.factory.clients:
+        #    c.sendLine("<{}> {}".format(self.transport.getHost(),
+        #               "Test message"))
 
     def connectionLost(self, reason):
         self.factory.clients.remove(self)
+        print "Disconnected for reason: " + str(reason) + "\nConnected clients: " + str(self.factory.clients)
 
-    def lineReceived(self, line):
+    def dataReceived(self, data):
+        print "Received data: " + str(data)
         for c in self.factory.clients:
-            c.sendLine("<{}> {}".format(self.transport.getHost(), line))
+            c.sendLine("<{}> {}".format(self.transport.getHost(), data))
+
+
 
 """Factory for DashboardProtocol class.
 Each instance will store its own unique set of connected clients."""
