b14b786057751e3948ed4278a3053b4cb3c012cd
Team1ASESSWENG500 <tch161@psu.edu>
2014-07-20 10:42:13 -0400

    Committing bugfixes
    Fixed email validation and spelling issues.

--- ChatDashboard/chatapp/static/validation.js
+++ ChatDashboard/chatapp/static/validation.js
@@ -12,10 +12,11 @@ function validatePassword(password) {
 }
 
 function validateEmail(email) {
-    var passpattern = /[^@]+@[^@]+\.[^@]+/;///^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    //var passpattern = /[^@]+@[^@]+\.[^@]+/;///^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var emailpattern = ^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$;
     var pos = 0;
 
-    pos = email.search(passpattern);
+    pos = email.search(emailpattern);
 
     if (pos === -1) {
         return false;
