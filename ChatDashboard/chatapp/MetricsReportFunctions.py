0ff3b1b98e979609bf3f787ccc70140268e1bad6
Team1ASESSWENG500 <tch161@psu.edu>
2014-08-03 22:36:15 -0400

    Added metrics report sub.

--- ChatDashboard/chatapp/MetricsReportFunctions.py
+++ ChatDashboard/chatapp/MetricsReportFunctions.py
@@ -4,9 +4,18 @@ __author__ = 'Dave'
 
 def retrieve_dashboard_posts_by_month(start, end):
     month_metrics = []
-    # put code here
-    #query
 
+    temp_metric = dashboard_posts_metric()
+    temp_metric.month = "01/2014"
+    temp_metric.num_posts = 20
+
+    month_metrics.append(temp_metric)
+
+    temp_metric = dashboard_posts_metric()
+    temp_metric.month = "02/2014"
+    temp_metric.num_posts = 35
+
+    month_metrics.append(temp_metric)
 
     return month_metrics
 
